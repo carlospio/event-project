219a3eb77a85d6289236c7203427a9d49970861b
Carlos Pio <carlospiobenicio@gmail.com>
2022-06-21 22:16:04 -0300

    aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
+import { Event } from "./pages/Evento"
 
-import { gql, useQuery } from "@apollo/client"
-
-import { client } from "./lib/apollo"
-
-
-const GET_LESSIONS_QUERY = gql`
-
-  query {
-    lessons {
-      id
-      title
-
-      
-    }
-  }
-`
-
-
-
-interface Lesson {
-  id: string,
-  title: string,
-}
-
-function App() {
-  
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSIONS_QUERY)
-
-  console.log(data)
-
-  return (
-  <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title} </li>
-    })}
-  </ul>
+function App(){
+  return(
+    <Event />
   )
 }
-
 export default App
